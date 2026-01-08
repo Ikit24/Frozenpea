35c21a2a161fecfd2f6a42d02eeb2846b31da5b8
Attila <szszattila2@gmail.com>
2026-01-08 12:02:35 +0200

    window methods moved to separate file for better organization

--- main.go
+++ main.go
@@ -6,16 +6,8 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"image/color"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/driver"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/widget"
-	"fyne.io/fyne/v2/container"
-
-	"github.com/BurntSushi/xgb"
-	"github.com/BurntSushi/xgb/xproto"
 )
 
 func main () {
@@ -70,74 +62,4 @@ func main () {
 	dummy.Resize(fyne.NewSize(1, 1))
 	dummy.Hide()
 	a.Run()
-}
-
-func showBreakWindow(a fyne.App) fyne.Window {
-	var w fyne.Window
-	fyne.DoAndWait(func() {
-		w = a.NewWindow("Break time!")
-
-		img := canvas.NewImageFromFile("./peakpx.jpg")
-		img.FillMode = canvas.ImageFillStretch
-		w.SetContent(img)
-		w.SetFullScreen(true)
-		w.SetCloseIntercept(func() {
-		})
-		w.Show()
-
-		if nativeWin, ok := w.(driver.NativeWindow); ok {
-			nativeWin.RunNative(func(ctx any) {
-				if x11ctx, ok := ctx.(driver.X11WindowContext); ok {
-					fmt.Println("Got X11 context, window:", x11ctx.WindowHandle)
-					conn, err := xgb.NewConn()
-					if err != nil {
-						fmt.Println("Connection failed:", err)
-						return
-					}
-
-					cookie := xproto.GrabKeyboard(conn, false, xproto.Window(x11ctx.WindowHandle), 0, xproto.GrabModeAsync, xproto.GrabModeAsync)
-					reply, err := cookie.Reply()
-					if err != nil {
-						fmt.Println("Cannot grab keyboard:", err)
-						return
-					}
-					if reply.Status != xproto.GrabStatusSuccess {
-						fmt.Println("Grab failed, status:", reply.Status)
-					}
-				}
-			})
-		} else {
-			fmt.Println("NOT a native window!")
-		}
-	})
-	return w
-}
-
-func showNotification(a fyne.App) fyne.Window {
-	var n fyne.Window
-	fyne.DoAndWait(func() {
-		n = a.NewWindow("Break in 1 minute!")
-		img := canvas.NewImageFromFile("./notify.jpg")
-		img.FillMode = canvas.ImageFillStretch
-
-		topText := canvas.NewText("Break starts in 1 minute! Save your work.", color.NRGBA{R: 255, G: 0, B: 0, A: 255})
-		topText.Alignment = fyne.TextAlignCenter
-		topText.TextSize = 24
-		topText.TextStyle = fyne.TextStyle{Bold: true}
-
-		textContainer := container.NewVBox(
-			topText,
-			widget.NewLabel(""),
-		)
-		
-		content := container.NewStack(img, container.NewBorder(textContainer, nil, nil, nil))
-
-		n.SetContent(content)
-		n.Resize(fyne.NewSize(1265, 650))
-		n.SetFixedSize(true)
-		n.SetCloseIntercept(func() {
-		})
-		n.Show()
-	})
-	return n
 }
