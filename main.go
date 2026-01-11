e16b6b230348f20944b1643ecbe27991784713e0
Attila <szszattila2@gmail.com>
2026-01-11 12:46:44 +0200

    add: break and work duration can now be set upon start

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"time"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
+type Config struct {
+	WorkDuration  string
+	BreakDuration string
+}
+
+var appConfig Config
+
 func main () {
 	a := app.New()
-	fmt.Println("Session is running.")
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -20,14 +27,24 @@ func main () {
 
 	go func() { <-sigs; done <- true} ()
 
-	isBreak := false
 	go func () {
-		time.Sleep(1 * time.Second)
+		time.Sleep(100 * time.Millisecond)
+		setupDone := make(chan bool)
+		startupWindow(a, setupDone)
+		<-setupDone
+
+		fmt.Println("Session is running.")
+		workMins, _ := strconv.Atoi(appConfig.WorkDuration)
+		workDur := time.Duration(workMins) * time.Minute
+		breakMins, _ := strconv.Atoi(appConfig.BreakDuration)
+		breakDur := time.Duration(breakMins) * time.Minute
+
+		isBreak := false
 		var w fyne.Window
 		mainLoop: for {
 			if isBreak {
 				select {
-				case <- time.After(10 * time.Minute):
+				case <- time.After(breakDur):
 					fmt.Println("Break is over, you can continue!")
 					fyne.Do(func() {
 					w.Close()
@@ -39,7 +56,7 @@ func main () {
 			} else {
 				var n fyne.Window
 				select {
-				case <- time.After(49 * time.Minute):
+				case <- time.After(workDur - 1 * time.Minute):
 					fmt.Println("Mandatory break starts in 1 minute! Make sure you save your work.")
 					n = showNotification(a)
 				case <- done:
